276b7578032d83d3629f7d7ee201bfda08d5c1be
RagManX <ragmanx@gamerdemos.com>
2025-08-19 18:31:16 -0500

    My completed Boot.Dev BookBot project

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import sys
+
 from stats import get_num_words
+from stats import char_counts
+from stats import sort_chars
 
 def get_book_text(filename):
     with open(filename) as f:
@@ -6,9 +10,35 @@ def get_book_text(filename):
     return file_contents
 
 def main():
-    contents = (get_book_text("./books/frankenstein.txt"))
-    counter = get_num_words(contents)
-    print (f'{counter} words found in the document')
+    book_to_sort = "./books/frankenstein.txt"
+    arguments = sys.argv
+    arguments.pop(0)
+    if not (arguments):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    while(arguments):
+        book_to_sort = arguments.pop(0)
+        contents = (get_book_text(book_to_sort))
+        counter = get_num_words(contents)
+        #print (f'{counter} words found in the document')
+        letters = char_counts(contents)
+        results = sort_chars(letters)
+        #print(results)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book_to_sort}...")
+        print("----------- Word Count ----------")
+        print(f"Found {counter} total words")
+        print("--------- Character Count -------")
+        #print(type(results))
+        while (results):
+            #print(results.pop(0))
+            item=results.pop(0)
+            if item["char"].isalpha():
+                print (f"{item["char"]}:", item["num"])
+        print("============= END ===============")
     return
 
 main()
+#print(sys.argv)
+#print(type(sys.argv))
+#print(len(sys.argv))
--- stats.py
+++ stats.py
@@ -2,3 +2,23 @@ def get_num_words(text_to_split):
     split_text = text_to_split.split()
     size = len(split_text)
     return size
+
+def char_counts(full_text):
+    text = full_text.lower()
+    chars = {}
+    for letter in text:
+        if letter in chars:
+            chars[letter] += 1
+        else:
+            chars[letter] = 1
+    return chars
+
+def sort_on(items):
+    return items["num"]
+
+def sort_chars(input_dict):
+    sorter = []
+    for letter in input_dict:
+        sorter.append({"char": letter, "num": input_dict[letter]})
+    sorter.sort(reverse=True, key=sort_on)
+    return sorter
